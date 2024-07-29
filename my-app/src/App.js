375d4c25ef15c5a5f7f8c1f21d12140d313aadf4
Vinnu <tharun05312@gmail.com>
2024-07-29 21:24:33 +0530

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Calculator from './Components/Calculator';
+
+function App(){
+  return (
+    <>
+    <Calculator/>
+  
+    </>
+  )
+}
+
+export default App;
